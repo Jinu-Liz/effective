a82df7bc444a4f0e5a8a860757784620f5efc8d3
Hyungseock-Yun <bagzein@gmail.com>
2024-02-08 23:57:08 +0900

    - 생성자 대신 정적 팩터리 메서드를 고려하라. > 정적 팩터리 메서드의 단점

--- src/main/java/practice/effective/chapter_01/item_01/staticfactory/bad/AdvancedSettings.java
+++ src/main/java/practice/effective/chapter_01/item_01/staticfactory/bad/AdvancedSettings.java
@@ -1,10 +1,7 @@
 package practice.effective.chapter_01.item_01.staticfactory.bad;
 
 /**
- * 정적 팩터리 메서드의 단점 1.
- * 상속은 사용하지 못하게 된다.
- *
- * 하지만, 합성해서 사용할 수 있다.
+ * 상속은 사용하지 못하지만 합성해서 사용할 수 있다.
  */
 public class AdvancedSettings {
 
--- src/main/java/practice/effective/chapter_01/item_01/staticfactory/bad/Settings.java
+++ src/main/java/practice/effective/chapter_01/item_01/staticfactory/bad/Settings.java
@@ -2,6 +2,10 @@ package practice.effective.chapter_01.item_01.staticfactory.bad;
 
 import practice.effective.chapter_01.item_01.staticfactory.good.before.Difficulty;
 
+/**
+ * 이 클래스의 인스턴스는 #getInstance()를 통해 사용한다.
+ * @see #getInstance()
+ */
 public class Settings {
 
   private boolean useAutoSteering;
@@ -15,10 +19,15 @@ public class Settings {
   private static final Settings SETTINGS = new Settings();
 
   /**
+   * 정적 팩터리 메서드의 단점
+   * 1.
    * 정적 팩터리만을 쓰게 만들려면 생성자를 private으로 만들어야 한다.
    * 즉, 상속을 사용하지 못하게 된다.
+   *
+   * 2.
+   * 메서드가 늘어나게 되면, 어떤 메서드가 인스턴스를 생성하는 메서드인지 찾기가 어렵다.
    */
-  public static Settings newInstance() {
+  public static Settings getInstance() {
     return SETTINGS;
   }
 
