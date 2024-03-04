0a6feeb0f25028d94389c06616d4813a93264b94
Hyungseock-Yun <bagzein@gmail.com>
2024-03-05 01:46:30 +0900

    - try-finally보다 try-with-resources를 사용하라. > 예외 발생 시 예외 표시 차이

---
+++ src/main/java/practice/effective/chapter_01/item_08/suppress/TopLine.java
@@ -0,0 +1,26 @@
+package practice.effective.chapter_01.item_08.suppress;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class TopLine {
+
+  /**
+   * 가장 마지막에 발생한 예외가 보인다.
+   * readLine에서 발생한 CharConversionException은 먹혀버려 보이지 않는다.
+   * 디버깅 할 때는 첫 예외가 중요한데, 이를 놓쳐버린다.
+   */
+  static String firstLineOfFile(String path) throws IOException {
+    BadBufferedReader br = new BadBufferedReader(new FileReader(path));
+    try {
+      return br.readLine();
+    } finally {
+      br.close();
+    }
+  }
+
+  public static void main(String[] args) throws IOException {
+    System.out.println(firstLineOfFile("book.obj"));
+  }
+}
---
+++ src/main/java/practice/effective/chapter_01/item_08/suppress/TopLineGood.java
@@ -0,0 +1,21 @@
+package practice.effective.chapter_01.item_08.suppress;
+
+import java.io.FileReader;
+import java.io.IOException;
+
+public class TopLineGood {
+
+  /**
+   * 첫 번째 예외도 보이고, 후속 예외들도 보인다.
+   * (Suppressed: java.io.StreamCorruptedException)
+   */
+  static String firstLineOfFile(String path) throws IOException {
+    try (BadBufferedReader br = new BadBufferedReader(new FileReader(path))) {
+      return br.readLine();
+    }
+  }
+
+  public static void main(String[] args) throws IOException {
+    System.out.println(firstLineOfFile("book.obj"));
+  }
+}
