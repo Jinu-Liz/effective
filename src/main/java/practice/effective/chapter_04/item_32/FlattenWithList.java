aeee01a69727fd8d907e04ff2226837c7190c47f
Hyungseock-Yun <bagzein@gmail.com>
2024-04-08 04:57:13 +0900

    - 제네릭과 가변인수를 함께 쓸 때는 신중하다. > 배열 -> List로 변경

--- src/main/java/practice/effective/chapter_04/item_32/FlattenWithList.java
+++ src/main/java/practice/effective/chapter_04/item_32/FlattenWithList.java
@@ -8,6 +8,7 @@ public class FlattenWithList {
 
   /**
    * 제네릭 varargs 매개변수를 List로 대체
+   * 해당 타입을 상속받는 타입들 모두 허용
    */
   static <T> List<T> flatten(List<List<? extends T>> lists) {
     List<T> result = new ArrayList<>();
@@ -19,9 +20,9 @@ public class FlattenWithList {
   }
 
   public static void main(String[] args) {
-    List<Integer> flatList = flatten(List.of(
+    List<Number> flatList = flatten(List.of(
       List.of(1, 2),
-      List.of(3, 4, 5),
+      List.of(3.0, 4.0, 5.2),
       List.of(6, 7))
     );
 
