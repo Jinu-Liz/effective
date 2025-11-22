a2cd240eb20cbbdbf33da42fa5461f7c3479b55c
jinuliz <dev.jinuliz@gmail.com>
2025-11-22 17:13:46 +0900

    - 명명 패턴보다 어노테이션을 사용하라 > 어노테이션 프로세서

---
+++ src/main/java/practice/effective/chapter_06/item39/annotation_processor/MyClass.java
@@ -0,0 +1,16 @@
+package practice.effective.chapter_06.item39.annotation_processor;
+
+import me.whiteship.log.Log;
+
+public class MyClass {
+
+  @Log
+  public void myMethod() {
+    System.out.println("Inside myMethod()");
+  }
+
+  public static void main(String[] args) {
+    MyClass myClass = new MyClass();
+    myClass.myMethod();
+  }
+}
